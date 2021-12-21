14fde8ea524244f8bd96cd988ada44a5dd865a5f
Eugene Kiselev <eugene.kiselev@financesoft.kg>
2021-12-21 19:08:15 +0600

    Create solution and add 2 c# projects

---
+++ AkvelonTestTask/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var referenceToAnotherProject = new Class1();
+        }
+    }
+}
